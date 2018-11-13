b59791ab8771e71e5af0103f0e5ac11a2408e0ac
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2018-11-13 13:45:05 +0100

    Adding exercises01

---
+++ material/exercises01/exersices01.js
@@ -0,0 +1,41 @@
+/*
+ * matmod exercises01
+ */
+
+mean = function(numbers) {
+    /**
+     * Calculate the mean of an array of numbers
+     * @param  {number[]} numbers A list of numbers
+     * @return {number} the mean of numbers with 2 decimal precision
+     */
+
+    // Todo: Write code here
+
+    return 0;
+};
+
+variance = function(numbers) {
+    /**
+     * Calculate the sample variance of an array of numbers
+     * @param  {number[]} numbers A list of numbers
+     * @return {number} the population variance of numbers with 2 decimal precision
+     */
+
+    // Todo: Write code here
+    return 0;
+};
+
+stddev = function(numbers) {
+    /**
+     * Calculate the sample standard deviation of an array of numbers
+     * @param  {number[]} numbers A list of numbers
+     * @return {number} the population variance of numbers with 2 decimal precision
+     */
+
+    // Todo: Write code here
+    return 0;
+};
+
+module.exports = mean;
+module.exports = variance;
+module.exports = stddev;
---
+++ material/exercises01/test/exercises01test.js
@@ -0,0 +1,20 @@
+const assert = require("chai").assert;
+const app = require("../exersices01").default;
+
+describe("Mean", function() {
+    it("mean of [1, 1, 1, 1, 3, 4, 5] should return 2.29", function() {
+        assert.equal(mean([1, 1, 1, 1, 3, 4, 5]), 2.29);
+    });
+});
+
+describe("Variance", function() {
+    it("Sample variance of [1,1,1,1,3,4,5,1,1,10,11] should return 13.87", function() {
+        assert.equal(variance([1, 1, 1, 1, 3, 4, 5, 1, 1, 10, 11]), 13.87);
+    });
+});
+
+describe("Standard deviation", function() {
+    it("Sample standard deviation of [1,2,-2,4,-3] should return 2.88", function() {
+        assert.equal(stddev([1, 2, -2, 4, -3]), 2.88);
+    });
+});
