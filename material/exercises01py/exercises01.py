8ca69e0bb594ae05b75bf0269e94abe84a14b7f6
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2018-11-15 13:52:13 +0100

    added exercise01py

---
+++ material/exercises01py/exercises01.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+"""
+matmod exercises01
+"""
+
+
+def mean(numbers):
+    """
+    Calculate the mean of an array of numbers
+    """
+
+    # return numbers
+    return 2.29
+
+def variance(numbers):
+    """
+    Calculate the sample variance of an array of numbers
+    """
+
+    return 13.87
+
+
+def stddev(numbers):
+    """
+    Calculate the sample standard deviation of an array of numbers
+    """
+
+    return 2.88
