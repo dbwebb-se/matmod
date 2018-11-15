1bff8a6f38c3fc9af7180aa8ea3def0c7e2bae19
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2018-11-15 14:35:11 +0100

    exercises02 python version

---
+++ material/exercises02py/exercises02.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+"""
+matmod exercises02
+"""
+
+
+def my_factorial(n):
+    """
+    Calculate the factorial of integer n
+
+    Args:
+        n: integer
+    Returns:
+        the factorial of n, n! = n*(n-1)*(n-2)*..*2*1
+    """
+
+    return 0
+
+def my_bin(n, N, p):
+    """
+    Calculate the probability binomial distribution Pr(X = x)
+
+    Args:
+        n: number of trials
+        N: size of population
+        p: probaility of success
+    Returns:
+        probability of exactly n successful trial with population N with
+        probability p with 3 decimal precision
+    """
+
+    return 0
+
+
+def cumulative_bin(n, N, p):
+    """
+    Calculate the probability binomial distribution Pr(X <= x)
+
+    Args:
+        n: number of trials
+        N: size of population
+        p: probaility of success
+    Returns:
+        probability of n or less successful trial with population N with
+        probability p with 3 decimal precision
+    """
+
+    return 0
+
+def exercise309():
+    """ Exercuse 309 """
+
+    #  En undersökning visar att en tredjedel av alla barn får huvudlöss.
+    #  Slumpmässigt väljer vi tre barn.
+    #  Vad är sannolikheten att exakt två av barnen av de valda barnen får huvudlöss?
+
+    #  Returnerna ditt svar med 3 decimaler
+
+    return 0
+
+def exercise_plane():
+    """ Exercise plane """
+
+    #  Ett flygbolag säljer 65 biljetter till ett flygplan som har kapacitet för
+    #  60 passagerare. Sannolikheten att en passagerare inte dyker upp är 0.1.
+    #  Vad är sannolikheten att flygplanet inte blir överbelastat?
+    #
+    #  Returnera svaret med 3 decimaler
+
+    return 0
