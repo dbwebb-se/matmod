ba961733ad6d2b6fe3cc7c99c5ab58505962338b
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2018-11-15 13:57:49 +0100

    updated docstrings in exercises01

--- material/exercises01py/exercises01.py
+++ material/exercises01py/exercises01.py
@@ -11,22 +11,37 @@ matmod exercises01
 def mean(numbers):
     """
     Calculate the mean of an array of numbers
+
+    Args:
+        numbers: iterable of numbers
+    Returns: 
+        the mean of numbers
     """
 
-    # return numbers
-    return 2.29
+
+    return 0
 
 def variance(numbers):
     """
     Calculate the sample variance of an array of numbers
+
+    Args:
+        numbers: iterable of numbers
+    Returns: 
+        the sample variance of numbers
     """
 
-    return 13.87
+    return 0
 
 
 def stddev(numbers):
     """
     Calculate the sample standard deviation of an array of numbers
+
+    Args:
+        numbers: iterable of numbers
+    Return: 
+        the sample standard deviation of numbers
     """
 
-    return 2.88
+    return 0
