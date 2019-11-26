63c922f483d87c21639afd9a161346d0bda7926d
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2019-11-26 16:45:18 +0100

    adding kmom02

---
+++ kursmoment/kmom02/kmom02.js
@@ -0,0 +1,220 @@
+/*
+ * MA1487 Matematisk modellering
+ * Kursmoment 02
+ */
+
+
+/**
+ * Exercise01
+ * Given two events A and B, such that
+ *
+ * Pr(A) = 0.16
+ * Pr(A ∩ B) = 0.03
+ * Pr(CA ∩ B) = 0.12
+ *
+ * Compute the probability Pr(A ∪ B) with 3 decimal precision
+ *
+ * @returns {Number} the probability Pr(A ∪ B) with 3 decimal precision
+ */
+function exercise01() {
+    return 0;
+}
+
+
+/**
+ * Exercise02
+ * Given two events A and B, such that
+ *
+ * Pr(A) = 0.16
+ * Pr(A ∩ B) = 0.03
+ * Pr(CA ∩ B) = 0.12
+ *
+ * Compute the probability Pr(B) with 3 decimal precision
+ *
+ * @returns {Number} the probability Pr(B) with 3 decimal precision
+ */
+function exercise02() {
+    return 0;
+}
+
+/**
+ * Exercise03
+ * Given two events A and B, such that
+ *
+ * Pr(A) = 0.16
+ * Pr(A ∩ B) = 0.03, and
+ * Pr(CA ∩ B) = 0.12
+ *
+ * Compute the probability Pr(A ∩ CB) with 3 decimal precision
+ *
+ * @returns {Number} the probability Pr(A ∩ CB) with 3 decimal precision
+ */
+function exercise03() {
+    return 0;
+}
+
+
+/**
+ * Exercise04
+ * Let A and B be events such that
+ *
+ * Pr(A) = x,
+ * Pr(B) = y, and
+ * Pr(A ∩ B) = z
+ *
+ * Write a function to determine the probability of Pr(CA ∩ CB),
+ * (the probability that neither of the events A nor B occurs)
+ * with 3 decimal precision
+ *
+ * @param {Number} x probability Pr(A)
+ * @param {Number} y probability Pr(B)
+ * @param {Number} z probability Pr(A ∩ B)
+ * @returns {Number} probability of Pr(CA ∩ CB) with 3 decimal precision
+ */
+function exercise04(x, y, z) {
+    return 0;
+}
+
+
+/**
+ * Exercise05
+ * Let A, B and C be event such that
+ *
+ * Pr(A) = 0.2
+ * Pr(B) = 0.3
+ * Pr(C) = 0.4
+ * The events A and B are independent
+ * The events A and C are independent
+ * The events B anc C can not occur simultaneously
+ *
+ * Compute and the return the probability that at
+ * least one event will occur (Pr(A ∪ B ∪ C) )with 3 decimal precision
+ *
+ * @returns {Number} the probability Pr(A ∪ B ∪ C) with 3 decimal precision
+ */
+function exercise05() {
+    return 0;
+}
+
+/**
+ * Exercise06
+ * Let A and B be event such that
+ * Pr(A) = x
+ * Pr(B) = y
+ * Pr(A ∩ B) = z
+ *
+ * Compute and the return the probability Pr(A|B) with 3 decimal precision
+ *
+ * @returns {Number} the probability Pr(A|B) with 3 decimal precision
+ */
+function exercise06(x, y, z) {
+    return 0;
+}
+
+/**
+ * Exercise07
+ * Let A and B be two events such that
+ * Pr(A ∩ B) = 0.212, and
+ * P(CA ∩ B) = 0.431
+ *
+ * Compute and return the probability Pr(A|B) with 3 decimal precision
+ *
+ *
+ * @returns {Number} the probability Pr(A|B) with 3 decimal precision
+ */
+function exercise07() {
+    return 0;
+}
+
+/**
+ * Exercise 08
+ * Let A and B be two events such that
+ * P(A) = 0.8
+ * P(A ∩ B) = 0.2, and
+ * P(A ∪ B) = 0.9.
+ *
+ * Compute and return the probability Pr(A|B) with 3 decimal precision
+ *
+ * @returns {Number} the probability Pr(A|B) with 3 decimal precision
+ */
+function exercise08() {
+    return 0;
+}
+
+/**
+ * Exercise09
+ * A regular 6-sided die is tossed twice.
+ *
+ * Compute and return the probability of an odd sum of the dots with
+ * 3 decimal precision given that the sum of the dots is 11 or greater.
+
+ *  @returns {Number} the probability of 2 dice showing an odd sum given
+ *                      given that the sum of the dots is 11 or greater
+ *                      with 3 decimal precision
+ *
+ */
+function exercise09() {
+    return 0;
+}
+
+/**
+ * Exercise10
+ * A gambler is playing a game where the probability of winning is x.
+ * Suppose the gambler is playing the game 10 times.
+ *
+ * Compute and return the probability that the gambler will win the game
+ * exactly n times with 3 decimal precision.
+ *
+ * @param {Number} x probability of winning the game
+ * @param {Number} n games won
+ * @returns {Number} probability that the gambler will win the game
+ *                     exactly n times with 3 decimal precision.
+ */
+function exercise10(x, n) {
+    return 0;
+}
+
+
+/**
+ * Exercise11 (Bayes' theorem)
+ *
+ * A fictional university offers three different programmes A,B, and C.
+ * Currently 30 students are enrolled in A, 60 in students in B, and 60 students in C.
+ *
+ * A student enrolled in the A-programme passes an exam with 0.6 probability
+ * A student enrolled in the B-programme passes an exam with 0.5 probability
+ * A student enrolled in the B-programme passes an exam with 0.4 probability
+ *
+ * Student X passed the exam.
+ * Compute and return the probaility student X is enrolled in the A-programme
+ * with 3 decimal precision
+ */
+function exercise11() {
+    return 0;
+}
+
+/**
+ * Exercise12 (Bayes' theorem)
+ * In a employee survey, the results shows that x% of the M male employee are satisfied
+ * with their salary, and y% of the F female employee are satisfied with their salary.
+ *
+ * Compute and return the probability a satisfied person is a female
+ *
+ */
+function exercise12(x, y, M, F) {
+    return 0;
+}
+
+module.exports.exercise01 = exercise01;
+module.exports.exercise02 = exercise02;
+module.exports.exercise03 = exercise03;
+module.exports.exercise04 = exercise04;
+module.exports.exercise05 = exercise05;
+module.exports.exercise06 = exercise06;
+module.exports.exercise07 = exercise07;
+module.exports.exercise08 = exercise08;
+module.exports.exercise09 = exercise09;
+module.exports.exercise10 = exercise10;
+module.exports.exercise11 = exercise11;
+module.exports.exercise12 = exercise12;
+
